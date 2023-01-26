e01a0aefbec5ef82fd357e11cb2ccd401897c1de
aymen2782 <aymen.sellaouti@gmail.com>
2023-01-26 16:56:25 +0100

    authService

---
+++ src/app/services/auth.service.spec.ts
@@ -0,0 +1,18 @@
+import { AuthService } from './auth.service';
+
+describe('AuthService', () => {
+  let authService: AuthService;
+
+  beforeEach(() => {
+    authService = new AuthService();
+  });
+  it('should be created', () => {
+    pending();
+  });
+  it('return true when user is logged In', () => {
+    pending();
+  });
+  it('return false when user is logged Out', () => {
+    pending();
+  });
+});
