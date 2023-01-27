9aa1040ebfc9da4bc4a4e002b41f1d68b9d45564
aymen2782 <aymen.sellaouti@gmail.com>
2023-01-27 13:26:58 +0100

    add helpers

--- src/app/components/counter/counter.component.spec.ts
+++ src/app/components/counter/counter.component.spec.ts
@@ -1,23 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CounterComponent } from './counter.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { findEl } from '../../helpers/tests/test.helpers';
 
-describe('CounterComponent', () => {
+fdescribe('CounterComponent', () => {
   let component: CounterComponent;
   let fixture: ComponentFixture<CounterComponent>;
-
+  let el: DebugElement;
+  let countShow: DebugElement;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CounterComponent ]
-    })
-    .compileComponents();
+      declarations: [CounterComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CounterComponent);
     component = fixture.componentInstance;
+    el = fixture.debugElement;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('CounterComponent exist', () => {
     expect(component).toBeTruthy();
+  });
+  it('increment counter by one when clic on + button', () => {
+    //Act
+    countShow = findEl(fixture, 'count');
+    fixture.detectChanges();
+    expect(true).toBe(true);
   });
 });
