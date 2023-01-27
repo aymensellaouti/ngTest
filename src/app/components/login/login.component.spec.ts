23f30742aca8d08a7ad1427b557b8fce1d4f46ed
aymen2782 <aymen.sellaouti@gmail.com>
2023-01-27 11:30:51 +0100

    add Login Component spec test example 1

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -2,26 +2,44 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { AuthService } from '../../services/auth.service';
 
 fdescribe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-  let debugElement: DebugElement
+  let debugElement: DebugElement;
+  let authServiceSpy: any;
+  let authService: AuthService;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
-
+      declarations: [LoginComponent],
+      providers: [AuthService],
+    }).compileComponents();
+    authService = TestBed.inject(AuthService);
+    authServiceSpy = spyOn(authService, 'isAuthentified');
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     debugElement = fixture.debugElement;
-    console.log({debugElement});
-
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('show USER Logged in <h1> if the user is logged', () => {
+    authServiceSpy.and.returnValue(true);
+    component.login();
+    fixture.detectChanges();
+    const h1: DebugElement = debugElement.query(By.css('h1'));
+    console.log(h1);
+    expect(h1.nativeElement.innerHTML).toContain('User Logged');
+  });
+  it('show USER not Logged in <h1> if the user is not logged', () => {
+    authServiceSpy.and.returnValue(false);
+    component.login();
+    fixture.detectChanges();
+    const h1: DebugElement = debugElement.query(By.css('h1'));
+    expect(h1.nativeElement.innerHTML).toContain('User not Logged');
   });
 });
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-
+  isLogged = true;
+  constructor(private authService: AuthService) {}
+  ngOnInit() {}
+  login() {
+    this.isLogged = this.authService.isAuthentified();
+  }
 }
